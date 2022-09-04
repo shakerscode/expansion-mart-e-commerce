592e0d8a3aecf11bb6d031b8761c48c30e81c2b6
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-09-04 19:59:05 +0600

    Had a very busy day. That's why I was unable to proceed work.  Now  I just got a few times and sat to write codes. Fixed newsletter issues. Now starting work with slider.

--- src/Components/Home/HomeComponents/NewsLetter.js
+++ src/Components/Home/HomeComponents/NewsLetter.js
@@ -9,12 +9,12 @@ const NewsLetter = () => {
                 <h4 className='md:text-3xl px-5 text-2xl font-bold text-white'>Join Our Newsletter And Get...</h4>
                 <p className='md:text-xl text-lg font-semibold text-black'>$20 discount for your first order</p>
                 <div className='flex text-white relative '>
-                    <AiOutlineMail className='absolute top-6 bottom-6 left-2 text-[40px] md:block hidden rounded-lg bg-primary px-3'/>
+                    <AiOutlineMail className='absolute top-6 bottom-6 left-4 text-[40px] md:block hidden rounded-lg bg-primary px-3'/>
                     <input 
                     type="email" 
                     placeholder='Enter your email'
                     className='md:w-[550px] mx-3 w-72 my-4 py-4 md:pl-16 md:pr-[8rem] pl-4 pr-12 text-black placeholder:text-gray rounded-lg focus:outline-none'/>
-                    <button className='flex justify-center items-center gap-2 absolute md:right-1 right-4 top-5 bottom-5 bg-secondary px-3 py-2 rounded-lg'><span className='md:block hidden'>Subscribe</span> <BsArrowRight/></button>
+                    <button className='flex justify-center items-center gap-2 absolute md:right-4 right-4 top-5 bottom-5 bg-secondary px-3 py-2 rounded-lg hover:bg-primary transition-all duration-500 ease-in-out hover:translate-x-1'><span className='md:block hidden'>Subscribe</span> <BsArrowRight/></button>
                 </div>
             </div>
         </div>
